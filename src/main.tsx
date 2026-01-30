1fdbddd2bf491d1712741a837ce3a354abb263a0
Gareth <garethdavieslive@gmail.com>
2026-01-30 14:34:25 +0000

    Project setup Vite, React, TypeScript and Carbon UI

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import '@carbon/styles/css/styles.css';
+import CoffeeApp from './CoffeeApp.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <CoffeeApp />
+  </StrictMode>,
+)
